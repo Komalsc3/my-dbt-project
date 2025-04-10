7b5929ceb3ca74badc843e8552d33ce4fb57a38c
Komal Singh <komalsc3@gmail.com>
2025-04-10 11:42:14 +0000

    phh_final

--- models/staging/ephe_phh_loandata.sql
+++ models/staging/ephe_phh_loandata.sql
@@ -1,7 +1,8 @@
 {{ config(materialized="ephemeral") }}
-with
-    random_table_master_loan_data_cte as (
-        {{ random_table_master_loan_data("raw.reverse_svcr_phh.vw_phh_loan_master") }}
-    )
+with random_table_master_loan_data_cte as (
+    {{ random_table_master_loan_data(source('raw_reverse_svcr_phh', 'vw_phh_loan_master')) }}
+)
+
 select *
 from random_table_master_loan_data_cte
+
